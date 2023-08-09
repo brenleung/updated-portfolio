1aa85c2c281ba9c9a940ad8d251394527196be7e
Brendan <brendansleung@gmail.com>
2023-08-09 00:35:12 -0700

    made font thicker, started navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './navbar.css';
 import logo from '../imgs/logo-trans.png';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
       <div className="navbar">
-        <img src={logo} alt="logo"></img>
+        <Link to="/"><img src={logo} alt="logo"></img></Link>
         <p>This website is a work in progress. Links are not functional at this time.</p>
+        {/* <Link to="/"><p>PROJECTS</p></Link>
+        <Link to="/"><p>ABOUT ME</p></Link>
+        <Link to="/"><p>RESUME</p></Link>
+        <Link to="/"><p>CONTACT ME</p></Link> */}
       </div>
     );
   }
