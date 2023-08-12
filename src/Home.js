d9d1c3efd04f78cd3708c627ecf3e279e7202127
Brendan <brendansleung@gmail.com>
2023-08-12 02:03:15 -0700

    added project boxes

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import './Home.css';
-import personal_photo from './imgs/photo.png';
+
 import { Link } from 'react-router-dom';
+import ProjectBox from './components/projectbox.js';
+
+import personal_photo from './imgs/photo.png';
+import gluchart from './imgs/gluchart.jpg';
+import zion from './imgs/zion.jpg';
+import ielc from './imgs/ielc.jpg';
+import age_predictor from './imgs/agepredictor.jpg';
 
 function Home() {
     return (
@@ -24,17 +31,17 @@ function Home() {
         <div className="projects">
             <div className="text-2">
                 <h3>Here is some of my best work:</h3>
-                <h2>GluChart</h2>
-                <p>Machine learning application used to determine future glucose levels. Completed in 24 hours at HackDavis. Won Best Hack for Social Good and Best Healthcare Hack.</p>
 
-                <h2>Zion Lutheran Redesign</h2>
-                <p>Redesigned the website of my alma mater using HTML, CSS, and JavaScript. Currently functional using Netlify.</p>
+                <Link to="/"><ProjectBox image={gluchart} title={"GluChart"} tag1={"REACT.JS"} tag2={"TEAM PROJECT"}
+                tag3={"AWARD WINNING"} description={"Machine learning application used to determine future glucose levels. Completed in 24 hours at HackDavis. Won Best Hack for Social Good and Best Healthcare Hack."}/></Link>
+                
+                <Link to="/"><ProjectBox image={zion} title={"Zion Lutheran Redesign"} tag1={"HTML"} tag2={"CSS"}
+                tag3={"USER-FRIENDLY"} description={"Redesigned the website of my alma mater using HTML, CSS, and JavaScript. Currently functional using Netlify."}/></Link>
 
-                <h2>IELC Website</h2>
-                <p>Website of a nonprofit located in Davis and was created by a team of fullstack developers in ~10 weeks.</p>
+                <Link to="/"><ProjectBox image={ielc} title={"Interactive Elementary Learning Center"} tag1={"REACT.JS"} tag2={"NODE.JS"}
+                tag3={"TEAM PROJECT"} description={"Website of a nonprofit located in Davis and was created by a team of fullstack developers in ~10 weeks. Was created as a part of the UC Davis organization #include."}/></Link>
 
-                <h2>Age Predictor</h2>
-                <p>Created in under two hours, this simple JavaScript application calls upon an API to gather an age prediction based on your name.</p>
+                <Link to="/"><ProjectBox image={age_predictor} title={"Age Predictor"} tag1={"REACT.JS"} tag2={"API"} tag3={"USER-FRIENDLY"} description={"Created in under two hours, this simple JavaScript application calls upon an API to gather an age prediction based on your name."}/></Link>
             </div>
         </div>
       </div>
---
+++ src/components/projectbox.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './projectbox.css';
+
+function ProjectBox({image, title, tag1, tag2, tag3, description}) {
+    return (
+      <div className="project-box">
+        <div className="proj-photo">
+            <img src={image} />
+        </div>
+        <div className="proj-text">
+            <h2>{title}</h2>
+            <div className="tags">
+                <p>{tag1}</p>
+                <p>{tag2}</p>
+                <p>{tag3}</p>
+            </div>
+            <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+  
+export default ProjectBox;
