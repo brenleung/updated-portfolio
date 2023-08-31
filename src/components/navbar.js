e3f8c884ebc95c77fe7f9f45ec054c60058d65e6
Brendan <brendansleung@gmail.com>
2023-08-31 12:07:28 -0700

    navbar mobile compatibility

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,25 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
 import logo from '../imgs/updated-logo.png';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false);  // default for menu being open is false
+
+    const navMobileClick = () => {
+        setMenuOpen(false);  // close menu upon clicking link
+    }
+
     return (
       <nav className="navbar">
-        <Link to="/"><img src={logo} alt="logo"></img></Link>
-        <p className="navbar-temp">This navbar is currently a WIP for mobile devices.</p>
-        <ul>
+        <div className="top-bar">
+            <Link to="/"><img src={logo} alt="logo"></img></Link>
+
+            <div className="menu" onClick={() => {setMenuOpen(!menuOpen)}}>
+                <FontAwesomeIcon icon={faBars} size="xl" />
+            </div>
+        </div>
+        
+        <ul className={menuOpen ? "open" : ""}>
             <li>
-                <Link to="/projects"><p>PROJECTS</p></Link>
+                <Link to="/projects" onClick={navMobileClick}><p>PROJECTS</p></Link>
             </li>
             <li>
-                <Link to="/about-me"><p>ABOUT ME</p></Link>
+                <Link to="/about-me" onClick={navMobileClick}><p>ABOUT ME</p></Link>
             </li>
             <li>
-                <Link to="/resume"><p>RESUME</p></Link>
+                <Link to="/resume" onClick={navMobileClick}><p>RESUME</p></Link>
             </li>
             <li>
-                <Link to="/contact"><p>CONTACT</p></Link>
+                <Link to="/contact" onClick={navMobileClick}><p>CONTACT</p></Link>
             </li>
         </ul>
       </nav>
