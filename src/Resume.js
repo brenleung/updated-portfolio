0f5f77aa1696e2d51792a75e99b50414309c91a4
Brendan <brendansleung@gmail.com>
2023-09-08 02:49:18 -0700

    changed resume page

--- src/Resume.js
+++ src/Resume.js
@@ -7,20 +7,10 @@ import whizkidz from '../src/imgs/whizkidz.png';
 function Resume() {
   return (
     <div className="resume">
-        <h3>Interactive Resume</h3>
-        <p>For a PDF version of my resume, click <a href={ResumePDF} target="_blank" rel="noreferrer">here</a>.</p>
-        <div className="resume-section">
-            <h2>WORK EXPERIENCE</h2>
-            <hr></hr>
-            <div className="resume-box">
-                <h3 className="year">2023</h3>
-                <div className="resume-box-2">
-                    <h3>WhizKidz Computer Center</h3>
-                    <h2>March 2023 - Present</h2>
-                    <p>Conducted small-group, weekly classes on introductory computer science concepts in Java, Python, and more, resulting in improved student comprehension at a young age. Designed and created supplementary lesson material using coding examples and slide decks to aid students effectively.</p>
-                </div>
-                <img src={whizkidz}></img>
-            </div>
+        <h3>Welcome to my resume.</h3>
+        <p>This page is currently undergoing a revamp. An interactive resume will be here soon.</p>
+        <div className="offlink">
+            <p>📃 For a PDF version of my resume, click <a href={ResumePDF} target="_blank" rel="noreferrer">here</a>.</p>
         </div>
     </div>
   );
